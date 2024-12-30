21a12cf06b84310c5c9fcae0d02292839ecebf57
Robert Marsal <me@robertmarsal.com>
2024-12-30 20:17:40 +0000

    fix: remove base path

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
-
-  basePath = `/${repo}`;
-}
-
 const nextConfig: NextConfig = {
   /**
    * Enable static exports for the App Router.
@@ -17,13 +7,6 @@ const nextConfig: NextConfig = {
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
   output: "export",
-
-  /**
-   * Set base path.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: basePath,
 
   /**
    * Disable server-based image optimization. Next.js does not support
