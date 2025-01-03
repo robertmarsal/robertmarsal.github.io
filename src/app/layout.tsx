f71b645df96abd6fbddae8e9ad066ff450a1c833
Robert Marsal <me@robertmarsal.com>
2025-01-03 14:00:05 +0000

    feat: update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Robert Marsal | Blog",
+  title: "Robert Marsal | Home",
 };
 
 export default function RootLayout({
